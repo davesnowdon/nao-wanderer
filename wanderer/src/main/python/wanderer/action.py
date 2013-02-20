da926412e6aa4222cb16bec8b52cff7fe580cf64
Dave Snowdon <dave@davesnowdon.com>
2013-02-20 07:44:50 +0000

    added JSON serialisation and tests for actions

--- wanderer/src/main/python/wanderer/action.py
+++ wanderer/src/main/python/wanderer/action.py
@@ -6,6 +6,8 @@ Created on Feb 4, 2013
 Representations of actions the robot can take
 '''
 
+from util.general import object_to_json
+
 '''
 Abstract class for all actions.
 Many actions will be implemented using choreographe boxes and should NOT
@@ -19,6 +21,19 @@ class Action(object):
     def name(self):
         return self.__class__.__name__
 
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    # used to support JSON serialisation of custom classes
+    def to_json(self):
+        return object_to_json(self, None)
+
+    # used to enable this class & sub types to be reconstituted from JSON
+    @classmethod
+    def from_json(klass, json_object):
+        return klass()
+
+
 '''
 Abstract base of any action that causes robot to move
 '''
@@ -26,24 +41,49 @@ class Move(Action):
     def __init__(self):
         super(Move, self).__init__()
 
+
 class WalkForwardsIndefinitely(Move):
     def __init__(self):
         super(WalkForwardsIndefinitely, self).__init__()
+
 
 class WalkStraight(Move):
     def __init__(self, distance):
         super(WalkStraight, self).__init__()
         self.distance = distance
 
+    def to_json(self):
+        return object_to_json(self, { 'distance': self.distance })
+
+    @classmethod
+    def from_json(klass, json_object):
+        return klass(json_object['distance'])
+
+
 class WalkSideways(Move):
     def __init__(self, distance):
         super(WalkSideways, self).__init__()
         self.distance = distance
 
+    def to_json(self):
+        return object_to_json(self, { 'distance': self.distance })
+
+    @classmethod
+    def from_json(klass, json_object):
+        return klass(json_object['distance'])
+
+
 class Turn(Move):
     def __init__(self, direction):
         super(Turn, self).__init__()
         self.direction = direction
+
+    def to_json(self):
+        return object_to_json(self, { 'direction': self.direction })
+
+    @classmethod
+    def from_json(klass, json_object):
+        return klass(json_object['direction'])
 
 '''
 Point at a location
@@ -54,12 +94,20 @@ class Point(Action):
         self.x = x
         self.y = y
 
+    def to_json(self):
+        return object_to_json(self, { 'x': self.x, 'y' : self.y })
+
+    @classmethod
+    def from_json(klass, json_object):
+        return klass(json_object['x'], json_object['y'])
+
 '''
 Perform a wave action
 '''
 class Wave(Action):
     def __init__(self):
         super(Wave, self).__init__()
+
 
 '''
 Speak some text. The argument is the key to look up a localised string
@@ -70,6 +118,14 @@ class Say(Action):
         super(Say, self).__init__()
         self.textKey = textKey
 
+    def to_json(self):
+        return object_to_json(self, { 'textKey': self.textKey })
+
+    @classmethod
+    def from_json(klass, json_object):
+        return klass(json_object['textKey'])
+
+
 '''
 Wait for a specified number of seconds
 '''
@@ -77,6 +133,14 @@ class Wait(Action):
     def __init__(self, delaySeconds):
         super(Wait, self).__init__()
         self.delaySeconds = delaySeconds
+
+    def to_json(self):
+        return object_to_json(self, { 'delaySeconds': self.delaySeconds })
+
+    @classmethod
+    def from_json(klass, json_object):
+        return klass(json_object['delaySeconds'])
+    
 
 '''
 Does nothing, used when we need an action but don't have anything to do
---
+++ wanderer/src/test/python/wanderer_tests/test_action.py
@@ -0,0 +1,69 @@
+'''
+Created on Feb 18, 2013
+
+@author: dsnowdon
+'''
+
+import unittest
+import math
+
+from util.general import to_json_string, from_json_string, object_to_name
+from wanderer.action import *
+
+class Test(unittest.TestCase):
+    def test_move_json(self):
+        action = Move()
+        self.json_serialisation(action)
+
+    def test_WalkForwardsIndefinitely_json(self):
+        action = WalkForwardsIndefinitely()
+        self.json_serialisation(action)
+
+    def test_WalkStraight_json(self):
+        action = WalkStraight(0.5)
+        self.json_serialisation(action)
+        action = WalkStraight(-0.5)
+        self.json_serialisation(action)
+
+    def test_WalkSideways_json(self):
+        action = WalkSideways(0.5)
+        self.json_serialisation(action)
+
+    def test_Turn_json(self):
+        action = Turn(math.pi/2)
+        self.json_serialisation(action)
+        action = Turn(-math.pi/2)
+        self.json_serialisation(action)
+
+    def test_Point_json(self):
+        action = Point(0.5, 0.75)
+        self.json_serialisation(action)
+
+    def test_Wave_json(self):
+        action = Wave()
+        self.json_serialisation(action)
+
+    def test_Say_json(self):
+        action = Say("Hello world")
+        self.json_serialisation(action)
+
+    def test_Wait_json(self):
+        action = Wait(0.1)
+        self.json_serialisation(action)
+        action = Wait(10)
+        self.json_serialisation(action)
+
+    def test_NullAction_json(self):
+        action = NullAction()
+        self.json_serialisation(action)
+
+    def json_serialisation(self, ev):
+        json = to_json_string(ev)
+        print "Serialisation of "+object_to_name(ev)+" = \n"+json
+        self.assertIsNotNone(json, "Serialised object should not be None")
+        rev = from_json_string(json)
+        self.assertTrue(isinstance(rev, ev.__class__))
+        self.assertEqual(ev, rev, "Reconstituted object "+repr(rev)+" must equal original "+repr(ev))
+
+if __name__ == '__main__':
+    unittest.main()
