18ef1c77413e123fb5aa63c82743d12268ddb0de
Dave Snowdon <dave@davesnowdon.com>
2013-05-02 07:39:14 +0100

    Removed to_degrees and to_radians as the built-in python functiosn math.degrees and math.radians should be used instead

--- wanderer/src/main/python/util/mathutil.py
+++ wanderer/src/main/python/util/mathutil.py
@@ -3,18 +3,10 @@ Created on Jan 19, 2013
 
 @author: dsnowdon
 '''
-import random
+
 import math
 
 FLOAT_CMP_ACCURACY=0.00000001
-
-def to_degrees(rad):
-    return rad * 180 / math.pi
-
-def to_radians(deg):
-    return deg * math.pi / 180
-
-
 
 def feq(a,b):
     return abs(a-b) < FLOAT_CMP_ACCURACY
