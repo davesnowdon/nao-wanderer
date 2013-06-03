377b15a9c35f258e25d760b70b8efdb4f1405689
Dave Snowdon <dave@davesnowdon.com>
2013-06-03 07:44:21 +0100

    test code to POST images and a server to receive them and save them to disk

---
+++ wanderer/src/test/python/data_collector/main.py
@@ -0,0 +1,81 @@
+'''
+Created on 19 May 2013
+
+Simple HTTP server to accept images and snesor data to assemble data sets
+
+@author: dsnowdon
+'''
+
+import sys
+import os
+import urlparse
+import cgi
+import BaseHTTPServer
+
+DATA_DIR=None
+PREFIX=None
+INDEX = 0
+
+class CollectorRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_POST(self):
+        global DATA_DIR
+        global PREFIX
+        global INDEX
+        print "got POST"
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers.getheader('content-length'))
+            postvars = urlparse.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        else:
+            postvars = {}
+        print postvars['sensor']
+        filedata = postvars['fileupload']
+        if (not filedata is None):
+            filename = "{prefix}-{index}.jpg".format(prefix=PREFIX, index=INDEX)
+            path = os.path.join(DATA_DIR, filename)
+            with open(path, "wb") as out:
+                out.write(''.join(filedata))
+            INDEX = INDEX + 1
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+
+
+class CollectorHTTPServer(BaseHTTPServer.HTTPServer):
+    def __init__(self, serverAddress, requestClass):
+        # HTTPServer is not a new style class
+        BaseHTTPServer.HTTPServer.__init__(self, serverAddress, requestClass)
+
+def make_server(port):
+    server_address = ('', port)
+    httpd = CollectorHTTPServer(server_address, CollectorRequestHandler)
+    return httpd
+
+def start_server(httpd):
+    httpd.serve_forever()
+
+def stop_server(httpd):
+    httpd.shutdown()
+
+def main(argv=None):
+    global DATA_DIR
+    global PREFIX
+    DATA_DIR = argv[1]
+    PREFIX = argv[2]
+    print "dir = {dir}, prefix = {prefix}".format(dir=DATA_DIR, prefix = PREFIX)
+    
+    if not os.path.exists(DATA_DIR):
+        try:
+            os.makedirs(DATA_DIR)
+        except OSError:
+            print("Failed to create dir: {}".format(DATA_DIR))
+            return 1
+    
+    httpd = make_server(8080)
+    start_server(httpd)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
